2b3492e483657511e7afe1e4506ba1abb25c3cfb
AlAl-kopin28980 <antofrancisca13@gmail.com>
2026-06-03 20:27:55 -0400

    fix: se arreglan errores obteniendo datos

--- main.cpp
+++ main.cpp
@@ -94,6 +94,7 @@ class LibroCreator{
 class Tree{
     private:
         Nodo root= Nodo();
+       
         void preorder(Nodo* nodo) {
             if (!nodo)
                 return;
@@ -136,14 +137,14 @@ class Tree{
             Nodo* similares=child->getHijo("similar_books");
             int i=0;
             for (Nodo* libro : similares->hijos){
-                Nodo* libro=child->getHijo( "publication_year");
-                int añoS= std::stoi( ((dato->hijos)[0])->data);
+                Nodo* date=libro->getHijo( "publication_year");
+                int añoS= std::stoi( ((date->hijos)[0])->data);
                 if (añoS>año){
                     i++;
                 }    
             }
             if (i==(similares->hijos).size()){
-            cout<<(child->getHijo("id"))->data<<endl;}
+            cout<<(((child->getHijo("id")))->hijos[0])->data<<endl;}
         }  
     }
 };
@@ -180,3 +181,102 @@ int main() {
     }
     return 0;
 }
+
+
+/*
+class Tree{
+    private:
+        Nodo root= Nodo();
+
+       Tree(){bool debugging = false;
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    //Lectura de Archivos XML, deben estar guardados en una carpeta llamada "XMLs" junto al ejecutable
+    namespace fs = std::filesystem;
+    string c = "XMLs/";
+    int i = 0;
+    LibroCreator lb = LibroCreator();
+    for (const auto & entry : fs::directory_iterator(c)){
+        pugi::xml_document doc;
+        pugi::xml_parse_result result = doc.load_file(entry.path().c_str());
+        if (!result)
+           break;
+        i++;
+      
+        lb.CrearNodo(&doc,&root);
+        if(debugging){
+            std::stack<Nodo*> testeo;
+            testeo.push(&root);
+            while (!testeo.empty()){
+                Nodo* auxNode = testeo.top();
+                testeo.pop();
+                cout<<auxNode->data<<endl;
+                for (Nodo* n: auxNode->hijos) testeo.push(n);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+        void preorder(Nodo* nodo) {
+            if (!nodo)
+                return;
+            if (nodo->data=="id"){
+                Nodo* n_id=(nodo->hijos)[0];
+                cout<<n_id->data<<endl;
+            }
+            for (Nodo* child : nodo->hijos)
+                preorder(child);
+        }
+        
+        void deleteSubtree(Nodo* node) {
+        if (!node) return;
+            for (auto child : node->hijos)
+                deleteSubtree(child);
+            delete node;
+        }
+        
+    public:
+     void listar(){
+            preorder(&root);
+        }
+
+    void borrar_ratings(float rating){
+        for (Nodo* child : root.hijos){
+            Nodo* dato=child->getHijo("average_rating");
+            string rat2= ((dato->hijos)[0])->data;
+            float num= std::stof(rat2);
+            if (num<=rating){
+                deleteSubtree(child);
+            }  
+        }                  
+    }
+
+    void precursores(){
+        for (Nodo* child : root.hijos){
+            Nodo* dato=child->getHijo( "publication_year");
+            string str= ((dato->hijos)[0])->data;
+            int año= std::stoi(str);
+            Nodo* similares=child->getHijo("similar_books");
+            int i=0;
+            for (Nodo* libro : similares->hijos){
+                Nodo* date=libro->getHijo( "publication_year");
+                int añoS= std::stoi( ((date->hijos)[0])->data);
+                if (añoS>año){
+                    i++;
+                }    
+            }
+            if (i==(similares->hijos).size()){
+            cout<<(((child->getHijo("id")))->hijos[0])->data<<endl;}
+        }  
+    }
+};
+
+
+*/
